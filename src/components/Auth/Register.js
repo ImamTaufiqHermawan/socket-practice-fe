af20f72f00845297e5c762fc836e60d26d571e05
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-09 11:27:48 +0700

    register component and styling with scss

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,8 +1,53 @@
 import React from 'react';
+import registerImage from '../../assets/images/register.svg';
+import { Link } from 'react-router-dom';
+
+import './Auth.scss';
 
 const Register = () => {
   return (
-    <h1>Register Screnn</h1>
+    <div id='auth-container'>
+      <div id='auth-card'>
+        <div className='card-shadow'>
+          <div id='image-section'>
+            <img src={registerImage} alt='Register' />
+          </div>
+
+          <div id='form-section'>
+            <h2>Create an account</h2>
+
+            <form>
+              <div className='input-field mb-2'>
+                <input placeholder='First name' />
+              </div>
+
+              <div className='input-field mb-2'>
+                <input placeholder='Last Name' />
+              </div>
+
+              <div className='input-field mb-2'>
+                <input placeholder='Email' />
+              </div>
+
+              <div className='input-field mb-2'>
+                <select>
+                  <option value='male'>Male</option>
+                  <option value='female'>Female</option>
+                </select>
+              </div>
+
+              <div className='input-field mb-2'>
+                <input placeholder='Password' />
+              </div>
+
+              <button>Login</button>
+            </form>
+
+            <p>already have an account? <Link to='/login'>Login</Link></p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
