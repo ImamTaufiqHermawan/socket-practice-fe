38e55e95609adb3832c4014803930131dcc829ef
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-09 11:30:18 +0700

    add link to register

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import loginImage from '../../assets/images/login.svg';
+import { Link } from 'react-router-dom';
 
 import './Auth.scss';
 
@@ -27,7 +28,7 @@ const Login = () => {
               <button>Login</button>
             </form>
 
-            <p>doesn't have account? Register here</p>
+            <p>doesn't have account? <Link to='/register'>Register here</Link></p>
           </div>
         </div>
       </div>
