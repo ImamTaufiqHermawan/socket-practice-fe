bef95138b3e1fd0f2e87140fb79ec6208f459179
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-09 10:35:35 +0700

    config react router dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import React from 'react';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
-import Chat from './components/Auth/Chat';
+import Chat from './components/Chat/Chat';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Chat/>
-      <Login/>
-      <Register/>
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Chat />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+          {/* <Route render={() => <h1>404 page not found</h1>} /> */}
+        </div>
+      </Router>
+    </>
   );
 }
 
