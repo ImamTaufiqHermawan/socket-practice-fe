f4fdc5a01c5b28790f9e2018f9622e9e2934ae45
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-16 14:38:46 +0700

    auto scrolling message box to bottom chat

--- src/components/Chat/components/MessageBox/MessageBox.js
+++ src/components/Chat/components/MessageBox/MessageBox.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import Message from "../Message/Message";
 
@@ -7,9 +7,21 @@ import './MessageBox.scss';
 const MessageBox = ({ chat }) => {
 
   const user = useSelector(state => state.authReducer.user);
+  const scrollBottom = useSelector(state => state.chatReducer.scrollBottom);
+  const msgBox = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      scrollManual(msgBox.current.scrollHeight)
+    }, 100)
+  }, [scrollBottom])
+
+  const scrollManual = (value) => {
+    msgBox.current.scrollTop = value
+  }
   
   return (
-    <div id='msg-box'>
+    <div id='msg-box' ref={msgBox}>
       {
         chat.Messages.map((message, index) => {
           return <Message
